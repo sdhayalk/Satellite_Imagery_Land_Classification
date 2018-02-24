f1b6380f5170206522affe9df5dcea13e9a2382e
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-02-24 03:55:11 -0700

    implemented basic data augmentation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,24 +2,44 @@ import cv2
 import numpy as np
 import os
 
+from PIL import Image
+
 class DataAugmentation:
 	def __init__(self, RESIZE_DIM):
 		self.RESIZE_DIM = RESIZE_DIM
-
-	def resize(self, image):
-		pass
-
+		
 
 	def flip_90(self, image):
-		pass
+		new_image = np.rot90(image)
+		new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+		return new_image
 
 
 	def flip_180(self, image):
-		pass
+		new_image = np.rot90(image)
+		new_image = np.rot90(new_image)
+		new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+		return new_image
 
 
 	def flip_270(self, image):
-		pass
+		new_image = np.rot90(image)
+		new_image = np.rot90(new_image)
+		new_image = np.rot90(new_image)
+		new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+		return new_image
+
+
+	def flip_vertically(self, image):
+		new_image = cv2.flip(image, 1)
+		new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+		return new_image
+
+
+	def flip_horizontally(self, image):
+		new_image = cv2.flip(image, 0)
+		new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+		return new_image
 
 
 	def random_rotate(self, image):
@@ -34,18 +54,65 @@ class DataAugmentation:
 		pass
 
 
-	
-
-
 class DataPreprocessing(DataAugmentation):
 
 	def __init__(self, DATA_DIR, RESIZE_DIM):
 		DataAugmentation.__init__(RESIZE_DIM)
 		self.DATA_DIR = DATA_DIR
 
-	def augment(self, augmentation_factor, flip_90=True, flip_180=True, flip_270=True, random_rotate=True, random_zoom=True, random_lightning=True):
-		for folder_name in os.listdir(DATA_DIR):
-			pass
+
+	def augment_and_convert_to_hdf5(self, augmentation_factor=1, flip_90=True, flip_180=True, flip_270=True, flip_vertically=True, flip_horizontally=True, random_rotate=True, random_zoom=True, random_lightning=True):
+		images_dataset = []
+		labels_dataset = []
+
+		for folder_name in os.listdir(self.DATA_DIR):
+			for file_name in os.listdir(self.DATA_DIR + os.sep + folder_name):
+				image = None
+				label = None
+				
+
+				if '.jpg' in file_name:
+					image = cv2.imread(self.DATA_DIR + os.sep + folder_name + os.sep + file_name)
+				elif '.tif' in file_name:
+					image = np.array(Image.open(self.DATA_DIR + os.sep + folder_name + os.sep + file_name))
+
+				image = cv2.resize(image, (self.RESIZE_DIM, self.RESIZE_DIM))
+				new_image = image.copy()
+				new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+				label = folder_name
+				images_dataset.append(new_image)
+				labels_dataset.append(label)
+
+				if flip_90:
+					new_image = self.flip_90(image.copy())
+					images_dataset.append(new_image)
+					labels_dataset.append(label)
+
+				if flip_180:
+					new_image = self.flip_180(image.copy())
+					images_dataset.append(new_image)
+					labels_dataset.append(label)
+
+				if flip_270:
+					new_image = self.flip_270(image.copy())
+					images_dataset.append(new_image)
+					labels_dataset.append(label)
+
+				if flip_vertically:
+					new_image = self.flip_vertically(image.copy())
+					images_dataset.append(new_image)
+					labels_dataset.append(label)
+
+				if flip_horizontally:
+					new_image = self.flip_horizontally(image.copy())
+					images_dataset.append(new_image)
+					labels_dataset.append(label)
+
+				
+
+
+
+			
 
 
 def main():
