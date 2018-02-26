0808fb4a411a4d07f93861422ff5f7a452632e57
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-02-25 21:43:57 -0700

    working v1

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,6 +5,7 @@ import h5py
 import copy
 
 from PIL import Image
+from random import shuffle
 
 class DataAugmentation:
 	def __init__(self, RESIZE_DIM):
@@ -64,108 +65,129 @@ class DataPreprocessing(DataAugmentation):
 
 
 	def augment_and_convert_to_hdf5(self, train_validation_counter, hdf5_train_filename, hdf5_validation_filename, augmentation_factor=1, flip_90=True, flip_180=True, flip_270=True, flip_vertically=True, flip_horizontally=True, random_rotate=True, random_zoom=True, random_lightning=True):
+		file_folder_list = []
+		f_train = []
+		f_test = []
+		f_train_data = []
+		f_train_label = []
+		f_test_data = []
+		f_test_label = []
+		first_flag = []
 		counter = 0
-		folder_count = -1
-		first_flag = True
+		round_robin_index = 0
 
-		for folder_name in os.listdir(self.DATA_DIR):
-			print("In folder", folder_name)
-			folder_count += 1
-			first_flag = True
-
-			f_train = h5py.File(hdf5_train_filename[:-5] + str(folder_count) + hdf5_train_filename[-5:], 'w')
-			f_test = h5py.File(hdf5_validation_filename[:-5] + str(folder_count) + hdf5_validation_filename[-5:], 'w')
+		for folder_name_index in range(0, len(os.listdir(self.DATA_DIR))):
+			folder_name = os.listdir(self.DATA_DIR)[folder_name_index]
+			
+			f_train.append( h5py.File(hdf5_train_filename[:-5] + str(folder_name_index) + hdf5_train_filename[-5:], 'w') )
+			f_test.append( h5py.File(hdf5_validation_filename[:-5] + str(folder_name_index) + hdf5_validation_filename[-5:], 'w') )
+			first_flag.append(True)
+			f_train_data.append(None)
+			f_train_label.append(None)
+			f_test_data.append(None)
+			f_test_label.append(None)
 
 			for file_name in os.listdir(self.DATA_DIR + os.sep + folder_name):
-				print(file_name)
-				image = None
-				images_train_dataset_temp = []
-				labels_train_dataset_temp = []
+				file_folder_list.append([folder_name_index, self.DATA_DIR + os.sep + folder_name + os.sep + file_name])
 
-				if '.jpg' in file_name:
-					image = cv2.imread(self.DATA_DIR + os.sep + folder_name + os.sep + file_name)
-				elif '.tif' in file_name:
-					image = np.array(Image.open(self.DATA_DIR + os.sep + folder_name + os.sep + file_name))
+		shuffle(file_folder_list)
 
-				image = cv2.resize(image, (self.RESIZE_DIM, self.RESIZE_DIM))
-				new_image = image.copy()
-				new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
-				label = folder_count
-				images_train_dataset_temp.append(new_image)
-				labels_train_dataset_temp.append(label)
+		for arr in file_folder_list:
+			folder_name_index = arr[0]
+			file_path = arr[1]
 
-				if flip_90:
-					new_image = self.flip_90(image.copy())
-					images_train_dataset_temp.append(new_image)
-					labels_train_dataset_temp.append(label)
+			print(file_path)
+			image = None
+			images_train_dataset_temp = []
+			labels_train_dataset_temp = []
 
-				if flip_180:
-					new_image = self.flip_180(image.copy())
-					images_train_dataset_temp.append(new_image)
-					labels_train_dataset_temp.append(label)
+			if '.jpg' in file_path:
+				image = cv2.imread(file_path)
+			elif '.tif' in file_path:
+				image = np.array(Image.open(file_path))
 
-				if flip_270:
-					new_image = self.flip_270(image.copy())
-					images_train_dataset_temp.append(new_image)
-					labels_train_dataset_temp.append(label)
+			image = cv2.resize(image, (self.RESIZE_DIM, self.RESIZE_DIM))
+			new_image = image.copy()
+			new_image = new_image.reshape((3, self.RESIZE_DIM, self.RESIZE_DIM))
+			label = folder_name_index
+			images_train_dataset_temp.append(new_image)
+			labels_train_dataset_temp.append(label)
 
-				# if flip_vertically:
-				# 	new_image = self.flip_vertically(image.copy())
-				# 	images_train_dataset_temp.append(new_image)
-				# 	labels_train_dataset_temp.append(label)
+			# if flip_90:
+			# 	new_image = self.flip_90(image.copy())
+			# 	images_train_dataset_temp.append(new_image)
+			# 	labels_train_dataset_temp.append(label)
 
-				# if flip_horizontally:
-				# 	new_image = self.flip_horizontally(image.copy())
-				# 	images_train_dataset_temp.append(new_image)
-				# 	labels_train_dataset_temp.append(label)
-				
-				images_train_dataset_temp = np.array(images_train_dataset_temp, dtype='float')
-				labels_train_dataset_temp = np.array(labels_train_dataset_temp, dtype='int')
-				images_train_dataset_temp = images_train_dataset_temp / 255.0	
+			# if flip_180:
+			# 	new_image = self.flip_180(image.copy())
+			# 	images_train_dataset_temp.append(new_image)
+			# 	labels_train_dataset_temp.append(label)
 
-				num_images = 4 #* len(os.listdir(self.DATA_DIR + os.sep + folder_name))
+			# if flip_270:
+			# 	new_image = self.flip_270(image.copy())
+			# 	images_train_dataset_temp.append(new_image)
+			# 	labels_train_dataset_temp.append(label)
 
-				if first_flag:
-					f_train_data = f_train.create_dataset("data", (num_images,3,224,224), maxshape=(None,3,224,224), chunks=(num_images,3,224,224))
-					f_train_label = f_train.create_dataset("label", (num_images,), maxshape=(None,), chunks=(num_images,))
-					f_test_data = f_test.create_dataset("data", (num_images,3,224,224), maxshape=(None,3,224,224), chunks=(num_images,3,224,224))
-					f_test_label = f_test.create_dataset("label", (num_images,), maxshape=(None,), chunks=(num_images,))
+			# if flip_vertically:
+			# 	new_image = self.flip_vertically(image.copy())
+			# 	images_train_dataset_temp.append(new_image)
+			# 	labels_train_dataset_temp.append(label)
 
-					try:
-						f_train_data[:] = images_train_dataset_temp
-						f_train_label[:] = labels_train_dataset_temp
-						f_test_data[:] = images_train_dataset_temp
-						f_test_label[:] = labels_train_dataset_temp
-					except:
-						pass
-					
-				else:
-					if counter != train_validation_counter:
-						f_train_data.resize(f_train_data.shape[0] + num_images, axis=0)
-						f_train_label.resize(f_train_label.shape[0] + num_images, axis=0)
-						f_train_data[-num_images:] = images_train_dataset_temp
-						f_train_label[-num_images:] = labels_train_dataset_temp
-						counter += 1
-					
-					else:
-						f_test_data.resize(f_test_data.shape[0] + num_images, axis=0)
-						f_test_label.resize(f_test_label.shape[0] + num_images, axis=0)
-						f_test_data[-num_images:] = images_train_dataset_temp
-						f_test_label[-num_images:] = labels_train_dataset_temp
-						counter = 0
-
-				first_flag = False
-				print('Saved image', file_name, '~ current trainhdf5 dshape:', f_train_data.shape)
-			print("Saved", folder_name,  "as HDF5")
-				
+			# if flip_horizontally:
+			# 	new_image = self.flip_horizontally(image.copy())
+			# 	images_train_dataset_temp.append(new_image)
+			# 	labels_train_dataset_temp.append(label)
 			
+			images_train_dataset_temp = np.array(images_train_dataset_temp, dtype='float')
+			labels_train_dataset_temp = np.array(labels_train_dataset_temp, dtype='int')
+			images_train_dataset_temp = images_train_dataset_temp / 255.0	
+
+			num_images = 1 #* len(os.listdir(self.DATA_DIR + os.sep + folder_name))
+
+			if first_flag[round_robin_index]:
+				f_train_data[round_robin_index] = f_train[round_robin_index].create_dataset("data", (num_images,3,224,224), maxshape=(None,3,224,224), chunks=(num_images,3,224,224))
+				f_train_label[round_robin_index] = f_train[round_robin_index].create_dataset("label", (num_images,), maxshape=(None,), chunks=(num_images,))
+				f_test_data[round_robin_index] = f_test[round_robin_index].create_dataset("data", (num_images,3,224,224), maxshape=(None,3,224,224), chunks=(num_images,3,224,224))
+				f_test_label[round_robin_index] = f_test[round_robin_index].create_dataset("label", (num_images,), maxshape=(None,), chunks=(num_images,))
+
+				try:
+					f_train_data[round_robin_index][:] = images_train_dataset_temp
+					f_train_label[round_robin_index][:] = labels_train_dataset_temp
+					f_test_data[round_robin_index][:] = images_train_dataset_temp
+					f_test_label[round_robin_index][:] = labels_train_dataset_temp
+				except:
+					pass
+				
+			else:
+				if counter != train_validation_counter:
+					f_train_data[round_robin_index].resize(f_train_data[round_robin_index].shape[0] + num_images, axis=0)
+					f_train_label[round_robin_index].resize(f_train_label[round_robin_index].shape[0] + num_images, axis=0)
+					f_train_data[round_robin_index][-num_images:] = images_train_dataset_temp
+					f_train_label[round_robin_index][-num_images:] = labels_train_dataset_temp
+					counter += 1
+				
+				else:
+					f_test_data[round_robin_index].resize(f_test_data[round_robin_index].shape[0] + num_images, axis=0)
+					f_test_label[round_robin_index].resize(f_test_label[round_robin_index].shape[0] + num_images, axis=0)
+					f_test_data[round_robin_index][-num_images:] = images_train_dataset_temp
+					f_test_label[round_robin_index][-num_images:] = labels_train_dataset_temp
+					counter = 0
+
+			first_flag[round_robin_index] = False
+			print('Saved image', file_name, '~ current trainhdf5 dshape:', f_train_data[round_robin_index].shape)
+			round_robin_index += 1
+			if round_robin_index >= len(os.listdir(self.DATA_DIR)):
+				round_robin_index = 0
+
+
 
 
 def main():
-	DATA_DIR = 'G:/DL/satellite_imagery_land_classification/data/mydataset'
+	# DATA_DIR = 'G:/DL/satellite_imagery_land_classification/data/mydataset'
+	DATA_DIR = 'G:/DL/satellite_imagery_land_classification/data/UCMerced_LandUse/Images'
 	RESIZE_DIM = 224
-	hdf5_train_filename = 'G:/DL/satellite_imagery_land_classification/data/dataset_train.hdf5'
-	hdf5_validation_filename = 'G:/DL/satellite_imagery_land_classification/data/dataset_validation.hdf5'
+	hdf5_train_filename = 'G:/DL/satellite_imagery_land_classification/data/ucmerced_dataset_train.hdf5'
+	hdf5_validation_filename = 'G:/DL/satellite_imagery_land_classification/data/ucmerced_dataset_validation.hdf5'
 	train_validation_counter = 15
 
 
--- train.py
+++ train.py
@@ -3,9 +3,12 @@ import os
 
 
 class Training:
-	def __init__(self, CNN_NETWORK_PATH, CNN_SOLVER_PATH, USE_GPU=True):
+	def __init__(self, CNN_NETWORK_PATH, CNN_SOLVER_PATH, CAFFEMODEL_PATH, SOLVERSTATE_PATH, USE_GPU=True, resume_training=False):
 		self.CNN_NETWORK_PATH = CNN_NETWORK_PATH
 		self.CNN_SOLVER_PATH = CNN_SOLVER_PATH
+		self.CAFFEMODEL_PATH = CAFFEMODEL_PATH
+		self.SOLVERSTATE_PATH = SOLVERSTATE_PATH
+		self.resume_training = resume_training
 
 		if USE_GPU:
 		    caffe.set_device(0)
@@ -13,8 +16,11 @@ class Training:
 		else:
 		    caffe.set_mode_cpu()
 
-		self.net = caffe.Net(CNN_NETWORK_PATH, caffe.TRAIN)
-		self.solver = caffe.get_solver(CNN_SOLVER_PATH)
+		if self.resume_training:
+			self.net = caffe.Net(self.CNN_NETWORK_PATH, self.CAFFEMODEL_PATH, caffe.TRAIN)
+		else:
+			self.net = caffe.Net(self.CNN_NETWORK_PATH, caffe.TRAIN)
+		self.solver = caffe.get_solver(self.CNN_SOLVER_PATH)
 
 
 	def display_stats(self):
@@ -30,15 +36,20 @@ class Training:
 
 	def train(self):
 		self.display_stats()
+		if self.resume_training:
+			self.solver.restore(self.SOLVERSTATE_PATH)
 		self.solver.solve()
 
 
 def main():
 	CNN_NETWORK_PATH = "G:/DL/satellite_imagery_land_classification/Satellite_Imagery_Land_Classification/ResNet-50-train.prototxt"
 	CNN_SOLVER_PATH = "G:/DL/satellite_imagery_land_classification/Satellite_Imagery_Land_Classification/ResNet-50-solver.prototxt"
+	CAFFEMODEL_PATH =  "G:/DL/satellite_imagery_land_classification/data/snapshot_iter_6327.caffemodel"
+	SOLVERSTATE_PATH =  "G:/DL/satellite_imagery_land_classification/data/snapshot_iter_6327.solverstate"
 	USE_GPU = True
+	resume_training = True
 
-	training = Training(CNN_NETWORK_PATH, CNN_SOLVER_PATH, USE_GPU=USE_GPU)
+	training = Training(CNN_NETWORK_PATH, CNN_SOLVER_PATH, CAFFEMODEL_PATH, SOLVERSTATE_PATH, USE_GPU=USE_GPU, resume_training=resume_training)
 	training.train()
 
 
